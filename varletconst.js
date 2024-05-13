953c3299a17faf7e816960ebfda3b8e791123a2b
iyatgithub <163440576+iyatgithub@users.noreply.github.com>
2024-05-13 16:14:07 +0500

    Create varletconst.js

---
+++ varletconst.js
@@ -0,0 +1,7 @@
+var name = "IYAT";
+let age = 10; // we can modify it but can't decalre it. 
+const salary = 100; // we can't modify constant values and we can't use it again.  
+//age = 61; //we can't us it again
+//salary = 200;
+var name ="NAFEES"; // you can repeat/use it again and also modify var. type casting is also done in it. compile identifies the datatype itself. 
+console.log(name);
